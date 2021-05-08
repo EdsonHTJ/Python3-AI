b43e0ac400b30417d0d53ee6bd956ac6b488dac8
Edson Holanda <edsonsobralense@gmail.com>
2021-05-08 19:27:13 -0300

    add output

--- Perceptron/main.py
+++ Perceptron/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from perceptron import Perceptron
+from perceptron import MLPerceptron
 import os
 
 
@@ -10,7 +10,7 @@ def post_process(arr):
     
     for i in range(len(arr)):
         s = 100*(arr[i]/array_sum)
-        arr[i] = 1 if s>50 else 0
+        arr[i] = 1 if s >= 50 else 0
         
     return arr
         
@@ -31,24 +31,44 @@ test_putputs = test_dataset[["d1","d2","d3"]].values
 
 plt.title("Eqm/Epochs")
 
-MLP = Perceptron(train_inputs, train_outputs, [15,3],learning_rate=0.1,precision=1e-6, post_processing=post_process)
+MLP = MLPerceptron(train_inputs, train_outputs, [15,3],learning_rate=0.1,precision=1e-6, post_processing=post_process)
 
 
 full_result = test_dataset
-for i in range(2):
+full_result = full_result.iloc[:, :-1]
+
     
-    MLP.train()
-    #plt.subplot(3,1,i+1)
-    plt.plot(MLP.eqms)
+MLP.train()
 
-    respostas=[]
-    for j in test_inputs:
-        respostas.append(MLP.evaluate(j))
 
-    test_result = pd.DataFrame(data=respostas,columns=[f'T{i+1},1',f'T{i+1},2',f'T{i+1},3'])
-    full_result = pd.concat([full_result,test_result],axis=1)
-        
-    plt.show()
+responses=[]
+for j in test_inputs:
+    responses.append(MLP.evaluate(j))
+
+test_result = pd.DataFrame(data=responses,columns=['T1','T2','T3'])
+full_result = pd.concat([full_result,test_result],axis=1)
+
+
+plt.plot(range(len(MLP.eqms)), MLP.eqms, "b-")
+
+i = 1
+pltPath = f"./results/plot{i}.png"
+while os.path.exists(pltPath):
+    i+=1
+    pltPath = f"./results/plot{i}.png"
+
+
+plt.savefig(pltPath)
+
+
+i = 1
+csvPath = f"./results/Resultado{i}.csv"
+
+while os.path.exists(csvPath):
+    i+=1
+    csvPath = f"./results/Resultado{i}.csv"
+
+full_result.to_csv(csvPath)
 
 
 print(full_result)
--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -3,122 +3,132 @@ import numpy as np
 from activation_function import sigmoid
 from activation_function import sigmoid_derivative
 
-class Perceptron:
-    
-    def __init__(self,input_values,output_values,layers,learning_rate=1e-2,
-                 precision=1e-6,activation_function=sigmoid,derivative_function=sigmoid_derivative,post_processing=None):
 
+class MLPerceptron:
 
-       ones_column = np.ones((len(input_values), 1)) * -1
-       self.input_values = np.append(ones_column, input_values, axis=1)
-       
-       self.output_values = output_values
-       self.learning_rate = learning_rate
-       self.precision = precision
-       self.activation_function = activation_function
-       self.derivative_function = derivative_function
-       self.post_processing = post_processing
+    def __init__(self, input_values, output_values, layers, learning_rate=1e-2,
+                 precision=1e-6, activation_function=sigmoid, derivative_function=sigmoid_derivative, post_processing=None):
 
-       self.I = []
-       self.Y = []
-       self.W = []
+        minus_col = np.ones((len(input_values), 1)) * -1
+        # Coloca a coluna de (-1) no inicio dos valores de Input
+        self.input_values = np.append(minus_col, input_values, axis=1)
+        self.output_values = output_values
+        self.learning_rate = learning_rate
+        self.precision = precision
+        self.post_processing = post_processing
 
-       n_input = self.input_values.shape[1]
+        self.G = activation_function
+        self.dG = derivative_function
 
-       for i in range(len(layers)):
-            self.W.append(np.random.rand(layers[i], n_input))
+        self.I = []
+        self.Y = []
+        self.W = []
+
+        self.W.append(np.random.rand(layers[0], len(self.input_values[0])))
+        self.I.append(np.zeros(layers[0]))
+        self.Y.append(np.zeros(layers[0]))
+
+        for i in range(1, len(layers)):
+            self.W.append(np.random.rand(layers[i], layers[i-1] + 1))
             self.I.append(np.zeros(layers[i]))
             self.Y.append(np.zeros(layers[i]))
-            n_input = layers[i] + 1
-    
-      
-       self.epochs = 0
-       self.eqms = []
 
+        self.epochs = 0
+        self.eqms = []
 
-    def evaluate(self,x):
+    def evaluate(self, input_eval):
 
+        output = self.full_propagation(np.append(-1, input_eval))
 
-        y = self.full_propagation(np.append(-1,x))
-        
-        
         if(self.post_processing != None):
-            y = self.post_processing(y)
+            output = self.post_processing(output)
+        return output
 
-
-        return y
-
-               
     def train(self):
 
         error = True
         eqm_actual = self.eqm()
-        
+
         while error:
+
             error = False
             eqm_previous = eqm_actual
-            for x, d in zip(self.input_values,self.output_values):
-
-                self.Y[-1] = self.full_propagation(x)
-                self.back_full(x,d)
+            
+            for x, d in zip(self.input_values, self.output_values):
                 
+                #realiza a predicao do valor de saida
+                self.Y[-1] = self.full_propagation(x)
+                
+                #realiza o ajuste na rede
+                self.back_propagation(x, d)
+
             eqm_actual = self.eqm()
             self.eqms.append(eqm_actual)
-            self.epochs+=1
-            if abs(eqm_actual-eqm_previous)>self.precision:
-                error=True
+            self.epochs += 1
 
-        print(self.epochs)
-        return self.eqms 
+            # check de precisao
+            if abs(eqm_actual-eqm_previous) > self.precision:
+                error = True
 
+        return self.eqms
 
-    def full_propagation(self,x):
-        Y=x.copy()
-        
-        for i,w in enumerate(self.W):
-                
+    def full_propagation(self, x):
+
+        Y = np.array(x)
+
+        for i, w in enumerate(self.W):
+
+            # Calcula In como sendo Wn * Yn-1
             self.I[i] = np.dot(w, Y)
-            Y    = self.activation_function(self.I[i])
-            
-            if i <len(self.W)-1:
-                Y = np.append(-1,Y)
-            self.Y[i]=Y
-            
+
+            # Atualiza o Yn obtendo G(I)
+            Y = self.G(self.I[i])
+
+            if i < len(self.W)-1:
+                # adiciona Y0 = -1
+                Y = np.append(-1, Y)
+
+            self.Y[i] = Y
+
         return self.Y[i]
-        
 
-    def back_layer(self,w,w_old,y,d,u,x,out):
-        if not out:
-            y= y[1:]
-            delta=sum(d*w_old)[1:]*self.derivative_function(u)
-            delta=delta.reshape(len(delta),1)
-        else:
-            delta=((d-y)*self.derivative_function(u))
-            delta=delta.reshape(len(delta),1)
-            
-        w += self.learning_rate*delta*x
+    def back_propagation(self, x_input, d):
 
-        return w,delta
+        cnt = 0
 
+        for i in reversed(range(len(self.W))):
 
-    def back_full(self,X,d):
-        out=True
-        w_old= 0
-        for i,w in reversed(list(enumerate(self.W))):
-            x=self.Y[i-1]
             if i == 0:
-                x=X
-            w,d = self.back_layer(w,w_old,self.Y[i],d,self.I[i],x,out)
-            out=False
-            w_old = self.W[i]
+                x = x_input
+            else:
+                x = self.Y[i-1]
 
+            y_aux = self.Y[i]
 
+            if i == len(self.W) - 1:
+                delta = ((d-y_aux)*self.dG(self.I[i]))
+            else:
+                delta = sum(d*self.W[i+1])[1:]*self.dG(self.I[i])
+
+            # realiza a transposta do delta
+            delta = np.array([delta]).T
+
+            # ajusta os pesos
+            self.W[i] += self.learning_rate*delta*x
+
+            # faz com que dn = delta(n+1)
+            d = delta
+            cnt += 1
 
     def eqm(self):
-        eq = 0
         
+        eq = 0
+
         for x, d in zip(self.input_values, self.output_values):
+
             Y = self.full_propagation(x)
             eq += 0.5 * sum((d - Y) ** 2)
-            
-        return eq/len(self.output_values)
+
+        eqm = eq/len(self.output_values)
+
+        return eqm
