d37f91115efbd6a963a3485889d8e94269a277f2
EdsonHTJ <edsonsobralense@gmail.com>
2021-06-15 01:25:37 -0300

    change to trampf

--- Fuzz/fuzz.py
+++ Fuzz/fuzz.py
@@ -7,11 +7,11 @@ umidade     = control.Antecedent(np.arange(0, 101, 1 ), 'umidade')
 
 variacao    = control.Consequent(np.arange(-15,16,1), 'variacao')
 
-temperatura['muito_frio']   = fuzz.trimf(temperatura.universe, [0,  0,  8])
-temperatura['frio']         = fuzz.trimf(temperatura.universe, [5, 10, 15])
-temperatura['ok']           = fuzz.trimf(temperatura.universe, [15,20, 25])
-temperatura['quente']       = fuzz.trimf(temperatura.universe, [25,35, 40])
-temperatura['muito_quente'] = fuzz.trimf(temperatura.universe, [35,50, 50])
+temperatura['muito_frio']   = fuzz.trimf(temperatura.universe,  [0,  0,  8])
+temperatura['frio']         = fuzz.trapmf(temperatura.universe, [5, 7,  13, 17])
+temperatura['ok']           = fuzz.trapmf(temperatura.universe, [12, 17,23, 27])
+temperatura['quente']       = fuzz.trapmf(temperatura.universe, [23, 27 ,38, 42])
+temperatura['muito_quente'] = fuzz.trimf(temperatura.universe,  [30,50, 50])
 
 umidade['seco']  = fuzz.trimf(umidade.universe, [0,   0,  50])
 umidade['bom']   = fuzz.trimf(umidade.universe, [0,  50, 100])
@@ -34,8 +34,8 @@ rule5 = control.Rule((temperatura['muito_quente']),  variacao['esfriar'])
 var_control = control.ControlSystem([rule1, rule2, rule3, rule4, rule5])
 var         = control.ControlSystemSimulation(var_control)
 
-var.input['temperatura'] = 30
-var.input['umidade'] = 50
+var.input['temperatura'] = 10
+var.input['umidade'] = 20
 
 var.compute()
 
